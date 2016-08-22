4fd075e89340143c382ab3bcc6f5d333ec3c9d31
betweenagrokandahardplace <michael.cousins@live.com.au>
2016-08-22 18:01:53 +1000

    Set up project
    Pseuodocode and logging

---
+++ TextFileRenamer/TextFileRenamer/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace TextFileRenamer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.txtFetchPath = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(293, 38);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(170, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Start renaming";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // txtFetchPath
+            // 
+            this.txtFetchPath.Location = new System.Drawing.Point(12, 12);
+            this.txtFetchPath.Name = "txtFetchPath";
+            this.txtFetchPath.Size = new System.Drawing.Size(737, 20);
+            this.txtFetchPath.TabIndex = 1;
+            this.txtFetchPath.WordWrap = false;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(13, 67);
+            this.txtLog.MaxLength = 32767000;
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(736, 182);
+            this.txtLog.TabIndex = 2;
+            this.txtLog.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 261);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtFetchPath);
+            this.Controls.Add(this.btnGo);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtFetchPath;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ TextFileRenamer/TextFileRenamer/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextFileRenamer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string fetchFolderPath = txtFetchPath.Text;
+            WriteTextToLog("We will fetch documents from path: " + fetchFolderPath);
+
+           /* foreach (textfile in folder)
+            * open text file
+            * read first string (make sure its not blank!)
+            * find the index of the known string "Published by Commonwealth of Australia"
+            * from that index work back 15 characters " - 21 Jan 2016 "
+            * remove spaces and dashes (we should now have 21Jan2016)
+            * set file name string to 2016-01-21 format
+            * save file in subfolder "\processed"
+            * end for each */
+            
+
+
+
+        }
+
+        internal int logNumber = 1;
+        internal void WriteTextToLog(string logText)
+        {
+            if (LogWriter(logText) == true) { } //we wrote to the log! woo!
+            else { MessageBox.Show("LOG WRITE ERRROR"); } //we didn't write to the log :(
+        }
+        internal bool LogWriter(string s) {
+            try {
+                txtLog.AppendText(logNumber.ToString() + "\t" + s + "\r\n");
+                logNumber++;
+                return true;
+            }
+            catch {
+                return false;
+            }
+        }
+    }
+}
